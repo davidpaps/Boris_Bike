019671d7176c68006f0de98266e94b1680491cc1
David Papamichael <davidpapamichael@gmail.com>
2019-12-12 16:47:46 +0000

    Step 17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,31 +2,35 @@ require_relative "bike"
 
 class DockingStation
 
+  attr_accessor :capacity
   attr_reader :bikes
+
   DEFAULT_CAPACITY = 20
   
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
     @bikes = []
   end
 
   def release_bike
     fail "No bikes avaliable" if empty?
-   @bikes.pop
+   bikes.pop
   end
 
   def dock(bike)
     fail "No room avaliable" if full?
-    @bikes << bike
+    bikes << bike
   end
 
+  
   private
-
+  
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    bikes.count >= capacity
   end
 
   def empty?
-    @bikes.empty?
+    bikes.empty?
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,14 +4,13 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    # was bike = subject.release_bike
     bike = Bike.new
     expect(bike).to be_working
-    # expect(bike.working?).to eq true
+    # same as - expect(bike.working?).to eq true
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
-    # 'is_expected_to' is shorter version of writing 'expect(subject).to'
+  # 'is_expected_to' is shorter version of writing 'expect(subject).to'
 
   it { is_expected.to respond_to(:bikes)}
 
@@ -26,8 +25,6 @@ describe DockingStation do
     expect(subject.bikes).to eq [bike]
   end
 
-
-  # Hashtag before a method name implies it is an instance method.
   describe "#release_bike" do
     it "releases a bike" do
       bike = Bike.new
@@ -36,18 +33,40 @@ describe DockingStation do
     end
   end
 
-  # When the station is empty
   describe "#release_bike" do
     it "Error when no bikes avaliable" do
       expect {subject.release_bike}.to raise_error "No bikes avaliable"
     end
   end
 
-describe "#dock" do
-  it "Error when dock full" do
-    DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}   # NAMESPACE OPERATOR , when calling a constant in rspec, qualify the constant with the class in which it is is declared. DockingStation::DEFAULT_CAPACITY
-    expect {subject.dock(Bike.new)}.to raise_error "No room avaliable"
+  describe "#dock" do
+    it "Error when dock full" do
+      subject.capacity.times {subject.dock(Bike.new)}   
+      expect {subject.dock(Bike.new)}.to raise_error "No room avaliable"
+      end
+   end
+
+# NAMESPACE OPERATOR , when calling a constant in rspec, qualify the constant 
+# with the class in which it is is declared. DockingStation::DEFAULT_CAPACITY
+
+  describe "initialization" do
+    subject { DockingStation.new }
+    let(:bike) {Bike.new}
+      it "defaults capacity" do
+       described_class::DEFAULT_CAPACITY.times do
+       subject.dock(bike) 
+       end
+    expect{ subject.dock(bike) }.to raise_error "No room avaliable"
+    end
   end
-end
+
+# Above has the default capacity instead. Below is the previous test.  
+# describe "initialize" do
+#   it "Capacity can change" do
+#     station = DockingStation.new(30)
+#     30.times { station.dock Bike.new }
+#     expect{ station.dock Bike.new }.to raise_error "No room avaliable"
+#   end
+ # end
 
 end
