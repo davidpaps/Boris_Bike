c78c742925bca75a6ad74297873be03b094deea9
David Papamichael <davidpapamichael@gmail.com>
2020-03-31 11:43:15 +0100

    docking_station_spec tests fixed and passing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,43 +2,35 @@ require "docking_station"
 
 describe DockingStation do
 
-  it "expects docking station to releases working bikes" do
-    bike = Bike.new
-    expect(bike).to be_working
-  end
-
- 
-  it "expects docking station to respond to release_bike method" do
-    expect(subject).to respond_to(:release_bike)
-  end
-
-  it "expects docking station to dock with 1 bike" do
-    expect(subject).to respond_to(:dock).with(1).argument
-  end
-
-  it "expects docking station to respond to bikes" do
-    expect(subject).to respond_to(:bikes)
-  end
-
-
- 
-
-  it "counts docked bikes" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes).to eq [bike]
-  end
-
   describe "#release_bike" do
+
+    it "expects docking station to releases working bikes" do
+      bike = Bike.new
+      expect(bike).to be_working
+    end
+
+    it "expects docking station to respond to release_bike method" do
+      expect(subject).to respond_to(:release_bike)
+    end
+
+    it "expects docking station to dock with 1 bike" do
+      expect(subject).to respond_to(:dock).with(1).argument
+    end
+  
+    it "expects docking station to respond to bikes" do
+      expect(subject).to respond_to(:bikes)
+    end
+
     it "releases a bike" do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
   
-      it "Error when no bikes avaliable" do
+    it "Error when no bikes avaliable" do
       expect {subject.release_bike}.to raise_error "No bikes avaliable"
     end
+
   end
 
   describe "#dock" do
@@ -53,6 +45,12 @@ describe DockingStation do
         expect(subject.dock(bike)).to eq [bike]
       end
 
+      it "counts docked bikes" do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.bikes).to eq [bike]
+      end
+
    end
 
 
@@ -61,18 +59,19 @@ describe DockingStation do
     subject { DockingStation.new }
     let(:bike) {Bike.new}
 
-      it "defaults capacity" do
-        described_class::DEFAULT_CAPACITY.times do
-        subject.dock(bike) 
-        end
-          expect{ subject.dock(bike) }.to raise_error "No room avaliable"
+    it "defaults capacity" do
+      described_class::DEFAULT_CAPACITY.times do
+      subject.dock(bike) 
       end
+        expect{ subject.dock(bike) }.to raise_error "No room avaliable"
+    end
 
-      it "Capacity can change" do
-        station = DockingStation.new(30)
-        30.times { station.dock Bike.new }
-        expect{ station.dock Bike.new }.to raise_error "No room avaliable"
-      end
+    it "Capacity can change" do
+      station = DockingStation.new(30)
+      30.times { station.dock Bike.new }
+      expect{ station.dock Bike.new }.to raise_error "No room avaliable"
+    end
+    
   end
 
 end
