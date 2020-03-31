d3dde6b948262aab400cc32ecb5fb8f5a388d640
David Papamichael <davidpapamichael@gmail.com>
2020-03-31 11:48:12 +0100

    bike_spec tests fixed and passing

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,7 +2,6 @@ require "bike"
 
 describe Bike do
 
-  # it { is_expected.to respond_to :working? }
   it "expects bikes to repspond to the method working?" do
     expect(subject).to respond_to(:working?)
   end
